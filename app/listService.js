f619546abd2e15ccdd23508e343fa4a3f517b17a
reedmohornSLC <reedmohornnc@gmail.com>
2017-05-03 09:59:32 -0600

    whattup

--- app/listService.js
+++ app/listService.js
@@ -1,4 +1,4 @@
-angular.module('billysAdoption').service('listService',function($https){
+angular.module('billysAdoption').service('listService',function($http){
   this.test1 = 'service is working'
   var key ="bdb4c5e9167b34db3cbd6f03a4f6f69c";
   var method='pet.find';
@@ -7,7 +7,7 @@ angular.module('billysAdoption').service('listService',function($https){
 
   this.petFinderHitter = function(animal,randomloc){
     //console.log('service')
-    return $https({
+    return $http({
       method:'GET',
       // url: 'http://api.petfinder.com/my.method?key=bdb4c5e9167b34db3cbd6f03a4f6f69c&format=json&animal=dog'
       url:"https://api.petfinder.com/" + method + "?key=" + key + "&format=json&animal="+animal+"&location="+randomloc
