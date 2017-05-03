85b7718b6ade5b96bda9b88dcc85cd934933bf03
reedmohornSLC <reedmohornnc@gmail.com>
2017-05-03 09:54:04 -0600

    fixed that https again

--- app/listService.js
+++ app/listService.js
@@ -10,7 +10,7 @@ angular.module('billysAdoption').service('listService',function($https){
     return $https({
       method:'GET',
       // url: 'http://api.petfinder.com/my.method?key=bdb4c5e9167b34db3cbd6f03a4f6f69c&format=json&animal=dog'
-      url:"http://api.petfinder.com/" + method + "?key=" + key + "&format=json&animal="+animal+"&location="+randomloc
+      url:"https://api.petfinder.com/" + method + "?key=" + key + "&format=json&animal="+animal+"&location="+randomloc
     }).then(function(response){
       console.log(response);
       return response.data;
